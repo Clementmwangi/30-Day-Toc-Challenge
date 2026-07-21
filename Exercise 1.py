b2c88f1c03d8f7e18b349a889b64c9a0db369bbf
Clement <clementmwangi520@gmail.com>
2026-07-21 12:26:00 +0300

    created a code asking for user input

---
+++ Exercise 1.py
@@ -0,0 +1,11 @@
+import sys
+
+def main():
+    #ask for user input 
+    name = input("What is your name: ")
+
+    print(f"Hello, {name}")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
