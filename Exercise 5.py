00b15ce8539562533a3d08bf7a59fdbbc831c9e4
Clement <clementmwangi520@gmail.com>
2026-07-21 15:50:07 +0300

    created some code to handle some division and quotient

--- Exercise 5.py
+++ Exercise 5.py
@@ -1,16 +1,27 @@
 """
+Ask the user for two integers and print:
 
+quotient using /
+integer division using //
+remainder using %
 """
 
 import sys
 
 def main():
 
+    #ask user for integers for each operation
+    num1 = int(input("Enter a number you want to be used: "))
+    num2 = int(input("Enter a number you want to be used: "))
+
+    num3 = num1 / num2
+    num4 = num1 // num2
+    num5 = num1 % num2
+
+    print(f"For the two integers you provided, when you divide {num1} by {num2} you get {num3}")
+    print(f"For the two integers you provided, when you divide {num1} by {num2} its rounded down to {num4}")
+    print(f"For the two integers you provided, when you divide {num1} by {num2} you get the remainder {num5}")
     return 0
-    pass
 
-if __name__ == "__main":
+if __name__ == "__main__":
     sys.exit(main())
-
-
-
