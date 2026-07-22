74465201db66a35efcaf1c0a6b6e6f542d93d011
Clement <clementmwangi520@gmail.com>
2026-07-22 09:32:15 +0300

    created some code to create a sentence and iterate over it 3 times

--- Exercise 9.py
+++ Exercise 9.py
@@ -1,13 +1,17 @@
 """
-
+Ask the user for a short message. Print it repeated three times.
 """
 
 import sys
 
 def main():
+    #ask user for a short message
+    message = input("Enter a short message you want to be shown: ")
 
+    for i in range(3):
+        print(message)
     return 0
-    pass
+    
 
 if __name__ == "__main__":
     sys.exit(main())
