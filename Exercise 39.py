1ee97e7b0fd5d80dacb718bc17731cf25111b764
Clement <clementmwangi520@gmail.com>
2026-07-23 22:17:38 +0300

    created some code to show only values

---
+++ Exercise 39.py
@@ -0,0 +1,16 @@
+"""
+Print the result of data.values().
+
+"""
+
+import sys
+
+def main():
+
+    data = {"a": 1, "b": 2, "c": 3}
+    
+    print(data.values())#shows the value only
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
