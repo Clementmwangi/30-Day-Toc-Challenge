135ef9bff13b38c5792e4d5ccf84f60a76a4c9fe
Clement <clementmwangi520@gmail.com>
2026-07-23 22:12:49 +0300

    created some code to count the number of keys in a dictionary

---
+++ Exercise 37.py
@@ -0,0 +1,18 @@
+"""
+data = {"a": 1, "b": 2, "c": 3}
+
+Print the number of key-value pairs
+"""
+
+import sys
+
+def main():
+
+    data = {"a": 1, "b": 2, "c": 3}
+
+    #used len as it will check the amount of keys
+    print(len(data))
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
