cbb64b6af204e5eb0c988288cb90b148d17cf80a
Clement <clementmwangi520@gmail.com>
2026-07-24 14:18:19 +0300

    created some code to take use for loops

---
+++ Exercise 56.py
@@ -0,0 +1,12 @@
+"""
+Ask for a word. Print whether the word starts with "a" or "A" using a boolean expression.
+"""
+
+import sys
+
+def main():
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
